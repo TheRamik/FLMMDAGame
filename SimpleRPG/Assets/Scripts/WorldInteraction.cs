a9e24535a577446a9baed36c3ae1daedf558b2f3
Ricky Tham <rickytham123@gmail.com>
2018-03-12 22:58:18 -0700

    new Game

---
+++ SimpleRPG/Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class WorldInteraction : MonoBehaviour {
+
+    NavMeshAgent playerAgent;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {
+            GetInteraction();
+        }
+    }
+
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if (interactedObject.tag == "Interactable Object")
+            {
+                Debug.Log("Interactable interacted");
+            }
+            else
+            {
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
+    }
+
+	
+
+}
